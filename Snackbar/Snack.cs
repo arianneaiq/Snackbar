c17ab7f7fc95bed0ab3d3227b5487890cb7cadec
arianneaiq <ariannexiang1217@gmail.com>
2022-06-10 11:15:54 +0200

    Add project files.

---
+++ Snackbar/Snack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snackbar
+{
+    internal class Snack
+    {
+        private double costPrice;
+        private bool isVegetarian;
+        private String name;
+        private int numberInStock;
+
+        public 
+    }
+}
